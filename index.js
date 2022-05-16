fb990eb625605519bfd04a83d48116fb3f071c04
Alizée <agillouaye@icloud.com>
2022-05-16 14:21:41 +0200

    Connexion bdd

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const cors = require("cors")
 require("dotenv").config()
 
 const PORT = process.env.PORT || 8800
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect( process.env.MONGODB_URI, {useNewUrlParser: true} )
+    .then(() => console.log("DB Connection Successful !"))
+    .catch((err) => console.log(err))
 
 app.listen(PORT, () => {
     console.log(`Backend server is listening on port ${PORT} !`)
