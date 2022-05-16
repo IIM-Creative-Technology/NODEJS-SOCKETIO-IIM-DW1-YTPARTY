e69cb44168b80dffb558b7752311db15a511f586
Quentin Guerrier <75quentinguerrier@gmail.com>
2022-05-16 14:22:11 +0200

    add video model

---
+++ mongoose/models/video.ts
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const VideoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+        unique: true
+    },
+}, 
+{
+    timestamps: true
+})
+
+module.exports  = mongoose.model("Video", VideoSchema)
