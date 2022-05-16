5862988afe54fe93fd58291ea934148f4100a0cb
Alizée <agillouaye@icloud.com>
2022-05-16 14:55:11 +0200

    Route inscription

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 const cors = require("cors")
 require("dotenv").config()
 
+const userRoute = require('./mongoose/routes/user')
+
 const PORT = process.env.PORT || 8800
 
 app.use(cors())
@@ -12,6 +14,8 @@ app.use(express.json())
 mongoose.connect( process.env.MONGODB_URI, {useNewUrlParser: true} )
     .then(() => console.log("DB Connection Successful !"))
     .catch((err) => console.log(err))
+
+app.use('/api/users', userRoute)
 
 app.listen(PORT, () => {
     console.log(`Backend server is listening on port ${PORT} !`)
